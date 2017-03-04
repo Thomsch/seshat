ab8f77c59d136f3f3a54655655f1322a3f492b1c
Thomsch <thms.sch@gmail.com>
2017-03-04 10:29:48 +0100

    Removed duplicate starting point for the application

--- src/ch/ceruleansands/seshat/Seshat.java
+++ src/ch/ceruleansands/seshat/Seshat.java
@@ -1,13 +1,35 @@
 package ch.ceruleansands.seshat;
 
+import ch.ceruleansands.seshat.gui.GuiFactory;
+import ch.ceruleansands.seshat.language.LanguageInitializer;
+import ch.ceruleansands.seshat.language.java.JavaModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
- * Loads the application.
- * @author Thomas Schweizer.
+ * Start point of the program.
+ * @author Thomsch
  */
-public class Seshat {
-    public static void main(String[] args) {
-        Application.launch(GuiLoader.class, args);
+public class Seshat extends Application {
+
+    private GuiFactory guiFactory;
+
+    @Override
+    public void init() throws Exception {
+        Injector injector = Guice.createInjector(new Module(), new JavaModule());
+        guiFactory = injector.getInstance(GuiFactory.class);
+
+        // TODO Will be changed and put in a happy world where Guice and JavaFX live together in harmony.
+        LanguageInitializer languageInitializer = injector.getInstance(LanguageInitializer.class);
+        languageInitializer.loadLanguages();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Editor editor = guiFactory.makeEditor(stage);
+        editor.configure();
+        editor.show();
     }
 }
